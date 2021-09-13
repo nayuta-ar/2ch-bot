a14b27e7df181127f560fac6a7ec5decaedd9a60
Tubuanha (つぶあん派) <70151015+tubuanha@users.noreply.github.com>
2021-09-13 02:22:06 +0000

    修正

--- index.js
+++ index.js
@@ -33,202 +33,210 @@ const con = createConnection({
 
 con.connect()
 
+process.on('uncaughtException', (error) => {
+  console.log(error)
+})
+
 client
   .once('ready', () => console.log(`${client.user.tag} でログインしました。`))
   .on('messageCreate', async (message) => {
     if (message.author.bot) return
 
-    const getUser = () => {
-      return new Promise((resolve) => {
-        con.query(
-          'SELECT * FROM `users` WHERE `userId` = ?',
-          [message.author.id],
-          (e, rows) => {
-            if (!rows[0]) {
-              con.query('INSERT INTO `users` (`userId`, `tag`) VALUES (?, ?)', [
-                message.author.id,
-                tagGen(),
-              ])
-
-              resolve({
-                userId: message.author.id,
-                nickName: '名無しさん',
-                tag: 'Unknown',
-                messageCount: 0,
-              })
-            } else {
-              con.query(
-                'UPDATE `users` SET `messageCount` = ? WHERE `userId` = ?',
-                [rows[0].messageCount + 1, message.author.id],
-              )
-
-              resolve(rows[0])
-            }
-          },
-        )
-      })
-    }
-
-    const userData = await getUser()
-
-    let userColor
-    if (userData.messageCount < 10) userColor = '#FFFFFF'
-    else if (userData.messageCount < 30) userColor = '#F4FFF4'
-    else if (userData.messageCount < 50) userColor = '#EAFFEA'
-    else if (userData.messageCount < 100) userColor = '#D5FFD5'
-    else if (userData.messageCount < 200) userColor = '#AAFFAA'
-    else if (userData.messageCount < 300) userColor = '#80FF80'
-    else if (userData.messageCount < 500) userColor = '#55FF55'
-    else if (userData.messageCount < 1000) userColor = '#2BFF2B'
-    else if (userData.messageCount < 1500) userColor = '#00FF00'
-    else if (userData.messageCount < 2000) userColor = '#00D500'
-    else if (userData.messageCount < 3000) userColor = '#00AA00'
-    else if (userData.messageCount < 5000) userColor = '#008000'
-    else userColor = '#005500'
-
-    if (
-      !message.member.roles.cache.some((role) => role.name === '常連') &&
-      userData.messageCount >= 100
-    )
-      message.member.roles.add(
-        message.guild.roles.cache.find((role) => role.name === '常連'),
-      )
-
-    if (message.member.roles.cache.some((role) => role.name === '運営'))
-      userData.nickName = `${userData.nickName}<:moderator:869939850638393374>`
-
-    if (
-      message.channel.type === 'GUILD_TEXT' &&
-      message.channel.parentId === '876368038528700436'
-    ) {
-      if (!message.content) {
-        return message
-          .reply('メッセージが取得できませんでした。\n再試行してください。')
-          .then((msg) => setTimeout(() => msg.delete(), 10000))
-      }
-
-      await message.delete()
-
-      const thStartMsg = await message.channel.send({
-        embeds: [
-          new MessageEmbed()
-            .setTitle(`${userData.nickName}(${userData.tag})`)
-            .setDescription(message.content)
-            .setColor(userColor),
-        ],
-      })
-      const createTh = await message.channel.threads.create({
-        name: message.content,
-        autoArchiveDuration: 1440,
-        startMessage: thStartMsg,
-      })
-
-      con.query('INSERT INTO `threads` (`threadId`, `ownerId`) VALUES (?, ?)', [
-        createTh.id,
-        message.author.id,
-      ])
-
-      await createTh.setRateLimitPerUser(3)
-
-      const addMsg = await createTh.send('Loading...')
-      await addMsg.edit(`${message.author}<@&875986483260043284>`)
-      await addMsg.delete()
-    }
-
-    if (message.channel.parentId === '870264227061989416')
-      return message.reply(
-        'こちらのスレはサービスを終了しました。\nスレ主の方は新しいスレを立ててください。',
-      )
-
-    if (
-      message.channel.type === 'GUILD_PUBLIC_THREAD' &&
-      message.channel.parent.parentId === '876368038528700436'
-    ) {
-      const getThread = () => {
+    try {
+      const getUser = () => {
         return new Promise((resolve) => {
           con.query(
-            'SELECT * FROM `threads` WHERE `threadId` = ?',
-            [message.channelId],
+            'SELECT * FROM `users` WHERE `userId` = ?',
+            [message.author.id],
             (e, rows) => {
-              con.query(
-                'UPDATE `threads` SET `resNum` = ? WHERE `threadId` = ?',
-                [rows[0].resNum + 1, message.channelId],
-              )
+              if (!rows[0]) {
+                con.query(
+                  'INSERT INTO `users` (`userId`, `tag`) VALUES (?, ?)',
+                  [message.author.id, tagGen()],
+                )
 
-              resolve(rows[0])
+                resolve({
+                  userId: message.author.id,
+                  nickName: '名無しさん',
+                  tag: 'Unknown',
+                  messageCount: 0,
+                })
+              } else {
+                con.query(
+                  'UPDATE `users` SET `messageCount` = ? WHERE `userId` = ?',
+                  [rows[0].messageCount + 1, message.author.id],
+                )
+
+                resolve(rows[0])
+              }
             },
           )
         })
       }
 
-      const threadData = await getThread()
+      const userData = await getUser()
 
-      if (threadData.defaultName && userData.nickName === '名無しさん')
-        userData.nickName = threadData.defaultName
+      let userColor
+      if (userData.messageCount < 10) userColor = '#FFFFFF'
+      else if (userData.messageCount < 30) userColor = '#F4FFF4'
+      else if (userData.messageCount < 50) userColor = '#EAFFEA'
+      else if (userData.messageCount < 100) userColor = '#D5FFD5'
+      else if (userData.messageCount < 200) userColor = '#AAFFAA'
+      else if (userData.messageCount < 300) userColor = '#80FF80'
+      else if (userData.messageCount < 500) userColor = '#55FF55'
+      else if (userData.messageCount < 1000) userColor = '#2BFF2B'
+      else if (userData.messageCount < 1500) userColor = '#00FF00'
+      else if (userData.messageCount < 2000) userColor = '#00D500'
+      else if (userData.messageCount < 3000) userColor = '#00AA00'
+      else if (userData.messageCount < 5000) userColor = '#008000'
+      else userColor = '#005500'
 
-      if (threadData.ownerId === message.author.id)
-        userData.nickName = `${userData.nickName}<:nushi:869905929146085396>`
-
-      let sendContent = message.content
-
-      if (message.reference) {
-        const m = await message.channel.messages
-          .fetch({ limit: 100 })
-          .then((msgs) =>
-            msgs
-              .filter((msg) => msg.id === message.reference.messageId)
-              .first(),
-          )
-
-        sendContent = `[>>${m.embeds[0].title.split(' ')[0]}](${m.url})\n${
-          message.content
-        }`
-      }
-
-      const embed = new MessageEmbed()
-        .setTitle(
-          `${threadData.resNum + 1} ${userData.nickName}(${userData.tag})`,
+      if (
+        !message.member.roles.cache.some((role) => role.name === '常連') &&
+        userData.messageCount >= 100
+      )
+        message.member.roles.add(
+          message.guild.roles.cache.find((role) => role.name === '常連'),
         )
-        .setDescription(sendContent)
-        .setColor(userColor)
 
-      let notImage = false
-      if (message.attachments.first()) {
-        if (message.attachments.first().contentType.includes('image/'))
-          embed.setImage(message.attachments.first().proxyURL)
-        else notImage = true
+      if (message.member.roles.cache.some((role) => role.name === '運営'))
+        userData.nickName = `${userData.nickName}<:moderator:869939850638393374>`
+
+      if (
+        message.channel.type === 'GUILD_TEXT' &&
+        message.channel.parentId === '876368038528700436'
+      ) {
+        if (!message.content) {
+          return message
+            .reply('メッセージが取得できませんでした。\n再試行してください。')
+            .then((msg) => setTimeout(() => msg.delete(), 10000))
+        }
+
+        await message.delete()
+
+        const thStartMsg = await message.channel.send({
+          embeds: [
+            new MessageEmbed()
+              .setTitle(`${userData.nickName}(${userData.tag})`)
+              .setDescription(message.content)
+              .setColor(userColor),
+          ],
+        })
+        const createTh = await message.channel.threads.create({
+          name: message.content,
+          autoArchiveDuration: 1440,
+          startMessage: thStartMsg,
+        })
+
+        con.query(
+          'INSERT INTO `threads` (`threadId`, `ownerId`) VALUES (?, ?)',
+          [createTh.id, message.author.id],
+        )
+
+        await createTh.setRateLimitPerUser(3)
+
+        const addMsg = await createTh.send('Loading...')
+        await addMsg.edit(`${message.author}<@&875986483260043284>`)
+        await addMsg.delete()
       }
 
-      await message.delete()
-      await message.channel
-        .send({
-          embeds: [embed],
-        })
-        .then((msg) => {
-          if (notImage)
-            msg.reply({
-              content: '添付ファイル',
-              files: [message.attachments.first()],
-            })
-        })
+      if (message.channel.parentId === '870264227061989416')
+        return message.reply(
+          'こちらのスレはサービスを終了しました。\nスレ主の方は新しいスレを立ててください。',
+        )
 
-      if (threadData.resNum + 1 >= 1000) {
-        message.channel
-          .send({
-            embeds: [
-              new MessageEmbed()
-                .setTitle('END')
-                .setDescription(
-                  'レス数が1000以上になったので書き込みを中止しました。\n新しいスレッドを立てて会話してください。',
+      if (
+        message.channel.type === 'GUILD_PUBLIC_THREAD' &&
+        message.channel.parent.parentId === '876368038528700436'
+      ) {
+        const getThread = () => {
+          return new Promise((resolve) => {
+            con.query(
+              'SELECT * FROM `threads` WHERE `threadId` = ?',
+              [message.channelId],
+              (e, rows) => {
+                con.query(
+                  'UPDATE `threads` SET `resNum` = ? WHERE `threadId` = ?',
+                  [rows[0].resNum + 1, message.channelId],
                 )
-                .setColor('RED'),
-            ],
+
+                resolve(rows[0])
+              },
+            )
           })
-          .then(() => {
-            message.channel.setLocked(true)
-            message.channel.setArchived(true)
+        }
+
+        const threadData = await getThread()
+
+        if (threadData.defaultName && userData.nickName === '名無しさん')
+          userData.nickName = threadData.defaultName
+
+        if (threadData.ownerId === message.author.id)
+          userData.nickName = `${userData.nickName}<:nushi:869905929146085396>`
+
+        let sendContent = message.content
+
+        if (message.reference) {
+          const m = await message.channel.messages
+            .fetch({ limit: 100 })
+            .then((msgs) =>
+              msgs
+                .filter((msg) => msg.id === message.reference.messageId)
+                .first(),
+            )
+
+          sendContent = `[>>${m.embeds[0].title.split(' ')[0]}](${m.url})\n${
+            message.content
+          }`
+        }
+
+        const embed = new MessageEmbed()
+          .setTitle(
+            `${threadData.resNum + 1} ${userData.nickName}(${userData.tag})`,
+          )
+          .setDescription(sendContent)
+          .setColor(userColor)
+
+        let notImage = false
+        if (message.attachments.first()) {
+          if (message.attachments.first().contentType.includes('image/'))
+            embed.setImage(message.attachments.first().proxyURL)
+          else notImage = true
+        }
+
+        await message.delete()
+        await message.channel
+          .send({
+            embeds: [embed],
           })
+          .then((msg) => {
+            if (notImage)
+              msg.reply({
+                content: '添付ファイル',
+                files: [message.attachments.first()],
+              })
+          })
+
+        if (threadData.resNum + 1 >= 1000) {
+          message.channel
+            .send({
+              embeds: [
+                new MessageEmbed()
+                  .setTitle('END')
+                  .setDescription(
+                    'レス数が1000以上になったので書き込みを中止しました。\n新しいスレッドを立てて会話してください。',
+                  )
+                  .setColor('RED'),
+              ],
+            })
+            .then(() => {
+              message.channel.setLocked(true)
+              message.channel.setArchived(true)
+            })
+        }
       }
+    } catch (e) {
+      return console.log(e)
     }
   })
   .on('guildMemberAdd', (member) => {
@@ -251,6 +259,11 @@ client
 
     switch (interaction.commandName) {
       case 'name': {
+        if (interaction.options.getString('name').length > 50) {
+          return interaction.editReply(
+            'ニックネームは50文字以内にしてください。',
+          )
+        }
         con.query(
           'SELECT * FROM `users` WHERE `userId` = ?',
           [interaction.user.id],
@@ -361,6 +374,11 @@ client
                 'このコマンドはスレ主のみ実行できます。',
               )
             else {
+              if (interaction.options.getString('name').length > 50) {
+                return interaction.editReply(
+                  'デフォルトハンドルは50文字以内にしてください。',
+                )
+              }
               con.query(
                 'UPDATE `threads` SET `defaultName` = ? WHERE `threadId` = ?',
                 [interaction.options.getString('name'), interaction.channelId],
